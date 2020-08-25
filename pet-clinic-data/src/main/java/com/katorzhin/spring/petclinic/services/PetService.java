b5307794bd4e2b89b2af5997c6833b13124def3a
Alex Katorzhin <alex.katorzhin@gmail.com>
2020-08-25 16:53:38 +0300

    refactoring to common crud interface

--- pet-clinic-data/src/main/java/com/katorzhin/spring/petclinic/services/PetService.java
+++ pet-clinic-data/src/main/java/com/katorzhin/spring/petclinic/services/PetService.java
@@ -1,15 +1,10 @@
 package com.katorzhin.spring.petclinic.services;
 
-import com.katorzhin.spring.petclinic.model.Owner;
+
 import com.katorzhin.spring.petclinic.model.Pet;
 
 import java.util.Set;
 
-public interface PetService {
+public interface PetService extends CrudService<Pet,Long> {
 
-    Pet findById(Long id);
-
-    Pet save(Pet owner);
-
-    Set<Pet> findAll();
 }
--- pet-clinic-data/src/main/java/com/katorzhin/spring/petclinic/services/VetService.java
+++ pet-clinic-data/src/main/java/com/katorzhin/spring/petclinic/services/VetService.java
@@ -2,13 +2,6 @@ package com.katorzhin.spring.petclinic.services;
 
 import com.katorzhin.spring.petclinic.model.Vet;
 
-import java.util.Set;
+public interface VetService extends CrudService<Vet,Long> {
 
-public interface VetService {
-
-    Vet findById(Long id);
-
-    Vet save(Vet owner);
-
-    Set<Vet> findAll();
 }
