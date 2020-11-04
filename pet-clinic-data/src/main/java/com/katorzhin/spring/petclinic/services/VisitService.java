0973bfaf48cd962f7cd36d485b1b162def7ef4b1
Alex Katorzhin <alex.katorzhin@gmail.com>
2020-11-04 21:48:25 +0200

    implemented map impl for visit service

---
+++ pet-clinic-data/src/main/java/com/katorzhin/spring/petclinic/services/VisitService.java
@@ -0,0 +1,7 @@
+package com.katorzhin.spring.petclinic.services;
+
+
+import com.katorzhin.spring.petclinic.model.Visit;
+
+public interface VisitService  extends CrudService<Visit,Long>{
+}
---
+++ pet-clinic-data/src/main/java/com/katorzhin/spring/petclinic/services/map/VisitMapService.java
@@ -0,0 +1,38 @@
+package com.katorzhin.spring.petclinic.services.map;
+
+import com.katorzhin.spring.petclinic.model.Visit;
+import com.katorzhin.spring.petclinic.services.VisitService;
+
+import java.util.Set;
+
+
+public class VisitMapService extends AbstractMapService<Visit, Long> implements VisitService {
+
+    @Override
+    public Set<Visit> findALL() {
+        return super.findAll();
+    }
+
+    @Override
+    public void deleteById(Long id) {
+        super.deleteById(id);
+    }
+
+    @Override
+    public Visit save(Visit visits) {
+        if (visits.getPet() == null || visits.getPet().getOwner() == null || visits.getPet().getId() == null) {
+            throw new RuntimeException("invalid visit");
+        }
+        return super.save(visits);
+    }
+
+    @Override
+    public void delete(Visit visits) {
+        super.delete(visits);
+    }
+
+    @Override
+    public Visit findById(Long id) {
+        return super.findById(id);
+    }
+}
