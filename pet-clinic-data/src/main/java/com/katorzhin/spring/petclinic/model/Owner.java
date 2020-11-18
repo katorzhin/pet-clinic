d582c48216b67606288581cab6544704ce6fcddd
Alex Katorzhin <alex.katorzhin@gmail.com>
2020-11-18 12:56:57 +0200

    refactored class

--- pet-clinic-data/src/main/java/com/katorzhin/spring/petclinic/model/Owner.java
+++ pet-clinic-data/src/main/java/com/katorzhin/spring/petclinic/model/Owner.java
@@ -9,11 +9,11 @@ import java.util.Set;
 @Setter
 @Getter
 @NoArgsConstructor
-
 @Entity
 @Table(name = "owners")
 public class Owner extends Person {
 
+    @Builder
     public Owner(Long id, String firstName, String lastName, String address,
                  String city, String telephone, Set<Pet> pets) {
         super(id, firstName, lastName);
