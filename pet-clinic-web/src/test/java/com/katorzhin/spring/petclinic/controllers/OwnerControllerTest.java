81c4f00ee1ee80c0b3dc2545ec75fcab13ee9b02
Alex Katorzhin <alex.katorzhin@gmail.com>
2020-11-22 18:39:50 +0200

    added OwnerControllerTest

--- pet-clinic-web/src/test/java/com/katorzhin/spring/petclinic/controllers/OwnerControllerTest.java
+++ pet-clinic-web/src/test/java/com/katorzhin/spring/petclinic/controllers/OwnerControllerTest.java
@@ -13,7 +13,7 @@ import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 
 import java.util.*;
 
-import static org.hamcrest.Matchers.hasSize;
+import static org.hamcrest.Matchers.*;
 import static org.mockito.Mockito.*;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
@@ -66,6 +66,15 @@ class OwnerControllerTest {
                 .andExpect(status().isOk())
                 .andExpect(view().name("notimplemented"));
             verifyZeroInteractions(ownerService);
+    }
+    @Test
+    void displayOwner()throws Exception{
+        when(ownerService.findById(anyLong())).thenReturn(Owner.builder().id(1L).build());
+
+        mockMvc.perform(get("/owners/123"))
+                .andExpect(status().isOk())
+                .andExpect(view().name("owners/ownerDetails"))
+                .andExpect(model().attribute("owner", hasProperty("id",is(1L))));
     }
 }
 
