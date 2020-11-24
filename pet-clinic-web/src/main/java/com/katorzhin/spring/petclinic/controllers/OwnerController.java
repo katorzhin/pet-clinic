99691bd687ea9b54998472622e05eea9b52cc1ce
Alex Katorzhin <alex.katorzhin@gmail.com>
2020-11-24 12:14:14 +0200

    added setAllowedFields

--- pet-clinic-web/src/main/java/com/katorzhin/spring/petclinic/controllers/OwnerController.java
+++ pet-clinic-web/src/main/java/com/katorzhin/spring/petclinic/controllers/OwnerController.java
@@ -3,7 +3,9 @@ package com.katorzhin.spring.petclinic.controllers;
 import com.katorzhin.spring.petclinic.services.OwnerService;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.WebDataBinder;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.InitBinder;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
@@ -18,6 +20,10 @@ public class OwnerController {
         this.ownerService = ownerService;
     }
 
+    @InitBinder
+    public void setAllowedFields(WebDataBinder dataBinder){
+        dataBinder.setAllowedFields("id");
+    }
     @RequestMapping({"", "/", "/index", "/index.html"})
     public String listOwners(Model model) {
         model.addAttribute("owners",ownerService.findALL());
