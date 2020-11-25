a7b355cabcfc0983d1ee6f3f685ca7bb05e8190a
Alex Katorzhin <alex.katorzhin@gmail.com>
2020-11-25 17:54:41 +0200

    impl find owner

--- pet-clinic-data/src/main/java/com/katorzhin/spring/petclinic/services/OwnerService.java
+++ pet-clinic-data/src/main/java/com/katorzhin/spring/petclinic/services/OwnerService.java
@@ -2,9 +2,13 @@ package com.katorzhin.spring.petclinic.services;
 
 import com.katorzhin.spring.petclinic.model.Owner;
 
+import java.util.List;
+
 public interface OwnerService extends CrudService<Owner, Long> {
 
     Owner findByLastName(String lastName);
+
+    List<Owner> findAllByLastNameLike(String lastName);
 
 
 }
--- pet-clinic-web/src/test/java/com/katorzhin/spring/petclinic/controllers/OwnerControllerTest.java
+++ pet-clinic-web/src/test/java/com/katorzhin/spring/petclinic/controllers/OwnerControllerTest.java
@@ -41,14 +41,14 @@ class OwnerControllerTest {
                 .build();
     }
 
-    @Test
-    void listOwners() throws Exception {
-        when(ownerService.findALL()).thenReturn(owners);
-
-        mockMvc.perform(get("/owners"))
-                .andExpect(view().name("owners/index"))
-                .andExpect(model().attribute("owners", hasSize(2)));
-    }
+//    @Test
+//    void listOwners() throws Exception {
+//        when(ownerService.findALL()).thenReturn(owners);
+//
+//        mockMvc.perform(get("/owners"))
+//                .andExpect(view().name("owners/index"))
+//                .andExpect(model().attribute("owners", hasSize(2)));
+//    }
 
     @Test
     void listOwnersByIndex() throws Exception {
@@ -64,9 +64,33 @@ class OwnerControllerTest {
     void findOwners() throws Exception {
         mockMvc.perform(get("/owners/find"))
                 .andExpect(status().isOk())
-                .andExpect(view().name("notimplemented"));
+                .andExpect(view().name("owners/findOwners"))
+                .andExpect(model().attributeExists("owner"));
+
             verifyZeroInteractions(ownerService);
     }
+
+    @Test
+    void processFindFormReturnMany() throws Exception {
+        when(ownerService.findAllByLastNameLike(anyString()))
+                .thenReturn(Arrays.asList(Owner.builder().id(1l).build(),
+                        Owner.builder().id(2l).build()));
+
+        mockMvc.perform(get("/owners"))
+                .andExpect(status().isOk())
+                .andExpect(view().name("owners/ownersList"))
+                .andExpect(model().attribute("selections", hasSize(2)));
+    }
+
+    @Test
+    void processFindFormReturnOne() throws Exception {
+        when(ownerService.findAllByLastNameLike(anyString())).thenReturn(Arrays.asList(Owner.builder().id(1l).build()));
+
+        mockMvc.perform(get("/owners"))
+                .andExpect(status().is3xxRedirection())
+                .andExpect(view().name("redirect:/owners/1"));
+    }
+
     @Test
     void displayOwner()throws Exception{
         when(ownerService.findById(anyLong())).thenReturn(Owner.builder().id(1L).build());
